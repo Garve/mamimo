24f79f20bb1d2d4046b309050912d95778fe950d
Garve <xgarve@gmail.com>
2022-12-02 21:48:06 +0100

    add new unit tests to the carryover module

--- tests/test_carryover.py
+++ tests/test_carryover.py
@@ -1,6 +1,9 @@
 """Test carryover."""
 
+import numpy as np
 import pytest
+from hypothesis import given
+from hypothesis.strategies import floats, lists
 from sklearn.utils.estimator_checks import check_estimator
 
 from mamimo.carryover import ExponentialCarryover, GeneralGaussianCarryover
@@ -16,3 +19,39 @@ from mamimo.carryover import ExponentialCarryover, GeneralGaussianCarryover
 def test_check_estimator(estimator):
     """Test if check_estimator passes."""
     check_estimator(estimator)
+
+
+@pytest.mark.parametrize(
+    "estimator",
+    [
+        ExponentialCarryover(),
+        GeneralGaussianCarryover(),
+    ],
+)
+@given(
+    inputs=lists(floats(min_value=0, max_value=1e30), min_size=1).map(
+        lambda x: np.array(x).reshape(-1, 1)
+    )
+)
+def test_output_is_greater_than_input(inputs, estimator):
+    """Test if carryover increases the values of the original input array."""
+    outputs = estimator.fit_transform(inputs)
+    assert (outputs >= inputs).all()
+
+
+@pytest.mark.parametrize(
+    "estimator",
+    [
+        ExponentialCarryover(),
+        GeneralGaussianCarryover(),
+    ],
+)
+@given(
+    inputs=lists(floats(min_value=0, max_value=1e30), min_size=1).map(
+        lambda x: np.array(x).reshape(-1, 1)
+    )
+)
+def test_output_is_equal_to_input_in_the_first_component(inputs, estimator):
+    """Test if carryover is equal to the input array in the first component."""
+    outputs = estimator.fit_transform(inputs)
+    assert outputs[0] == inputs[0]
